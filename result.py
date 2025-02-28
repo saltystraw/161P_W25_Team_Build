7eb76064fa2fe04b3516cf366c4b23c2535f770c
ryanr <ricerb@my.lanecc.edu>
2025-02-27 16:11:10 -0800

    Im going to add the result functions to the team building exercise.

--- result.py
+++ result.py
@@ -1,3 +1,5 @@
+from unittest import result
+
 from validate import get_valid_choice
 def display_result(result):
     """
@@ -17,3 +19,11 @@ def display_result(result):
     Display on a new line x+4 asterisks where x is the number of letters in result
     Display a blank line
     """
+    res_len = len(result)   # set to the length of result
+    border = "*" * (res_len + 4)  # x+4 asterisks where x is the number of letters in result
+    print(border)           # prints top row of border
+    print(f"  {result}  ")#prints result of functions
+    print(border)
+
+
+display_result("hello world!")
